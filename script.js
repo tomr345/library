f4c54239e01e493a0be1963b704fa0d92acc6f45
Tom Rintoul <thomas.rintoul@icloud.com>
2022-05-24 17:33:30 +0100

    figured how to toggle overlay

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function {
+const plus = document.getElementById("plus");
+const overlay = document.getElementById("overlay");
+
+plus.addEventListener('click', () => {
+    overlay.style.display = "block";
     
-}
+})
