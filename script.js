ccecf5f762041a67b1d283d1f1003d1d19585dd1
Tom Rintoul <thomas.rintoul@icloud.com>
2022-05-26 15:54:03 +0100

    got the read status buttons to toggle properly

--- script.js
+++ script.js
@@ -11,33 +11,117 @@ class Book {
 
 //buttons
 const plusBtn = document.getElementById("plus");
+plusBtn.addEventListener('click', addOverlay);
 const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener('click', addBookToShelf);
+let readStatusBtn;
+let readStatusButtonClasses;
 
-//variables need for adding books
+//elements need for adding books
 const overlay = document.getElementById("overlay");
 const newTitleField = document.getElementById("title");
 const newAuthorField = document.getElementById("author");
 const newPagesField = document.getElementById("pages");
-const isReadStatusField = document.querySelector('input[name="read-status"]:checked');
+// const isReadStatusField = document.querySelector('input[name="read-status"]:checked');
+const isReadStatusField = document.getElementsByName('read-status');
+//readStatus to store the value of the button
+let readStatus;
 
-plusBtn.addEventListener('click', () => {
-    overlay.style.display = "block";
-})
+function displayBook() {
+    const main = document.querySelector('.main');
+    const book = document.createElement('div');
+    book.classList.add('book');
 
-submitBtn.addEventListener('click', () => {
+    const title = document.createElement('p');
+    title.setAttribute('id', "bookTitle");
+    title.innerHTML = `${newTitleField.value}`;
+
+    const author = document.createElement('p');
+    author.setAttribute('id', "bookAuthor");
+    author.innerHTML = `${newAuthorField.value}`;
+
+    const pages = document.createElement('p');
+    pages.setAttribute('id', "bookPages");
+    pages.innerHTML = `${newPagesField.value} pages`;
+
+    const buttons = document.createElement('div');
+    buttons.classList.add('bookReadStatus');
+
+    const readBtn = document.createElement('button');
+    readBtn.innerHTML = `${readStatus}`;
+    if (readStatus === "Read") {
+        readBtn.classList.add('homepageBtn', 'read');
+    }
+    else {
+        readBtn.classList.add('homepageBtn', 'unread');
+    }
+    readBtn.setAttribute('id', "readStatus");
+    readBtn.addEventListener('click', toggleReadStatus);
+    buttons.appendChild(readBtn);
+
+    const remove = document.createElement('button');
+    remove.innerHTML = "Remove";
+    remove.classList.add('homepageBtn', 'remove');
+    buttons.appendChild(remove);
+
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(buttons);
+    main.appendChild(book);
+}
+
+function addBookToShelf() {
     const newTitle = newTitleField.value;
     const newAuthor = newAuthorField.value;
     const newPages = newPagesField.value;
-    const isReadStatus = isReadStatusField.value;
+    // const isReadStatus = isReadStatusField.value;
 
-    const book = new Book(newTitle, newAuthor, newPages, isReadStatus);
-    myLibrary.push(book);
-    //get rid of this at the end
-    for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i]);
+    for (let i = 0; i < isReadStatusField.length; i++) {
+        if (isReadStatusField[i].checked) {
+            readStatus = isReadStatusField[i].id;
+        } 
     }
+
+    //create nnew book
+    const book = new Book(newTitle, newAuthor, newPages, readStatus);
+    myLibrary.push(book);
+    console.log(myLibrary);
+
+    //add book to shelf and reset fields
+    displayBook();
     reset();
-})
+}
+
+function toggleReadStatus(e) {
+    const classes = e.target.className.split(' ');
+    if (classes.includes('read')) {
+        e.target.classList.remove('read');
+        e.target.classList.add('unread');
+        e.target.innerHTML = "Unread";
+    }
+    else {
+        e.target.classList.remove('unread');
+        e.target.classList.add('read');
+        e.target.innerHTML = "Read";
+    }
+    // readStatusBtn = document.getElementById('readStatus');
+    // readStatusButtonClasses = readStatusBtn.className.split(' ');
+    // if (readStatusButtonClasses.includes('read')) {
+    //     readStatusBtn.classList.remove('read');
+    //     readStatusBtn.classList.add('unread');
+    //     readStatusBtn.innerHTML = "Unread";
+    // }
+    // else {
+    //     readStatusBtn.classList.remove('unread');
+    //     readStatusBtn.classList.add('read');
+    //     readStatusBtn.innerHTML = "Read";
+    // }
+}
+
+function addOverlay() {
+    overlay.style.display = "block";
+}
 
 function reset() {
     overlay.style.display = "none";
